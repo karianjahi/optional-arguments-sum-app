f5baf2a2320679e4a083fe2f3c53846334576aeb
karianjahi <kitgaedesigaidi8@gmail.com>
2025-10-31 22:41:41 +0100

    Write the user stories

---
+++ source.js
@@ -0,0 +1,20 @@
+/*
+A function that accepts up to two arguments, and sum them, but if there is only one argument returns a function that waits for the second number to sum.
+User Stories:
+
+1. Create a function `addTogether`.
+
+2. If the function receives two arguments, the function returns the sum of the two arguments.
+
+3. If only one argument is provided, addTogether returns a function.
+
+    - When the returned function is called with a single argument, it returns the sum.
+                    `const sumTwoAnd = addTogether(2);
+                    sumTwoAnd(3); // 5`
+4. If either argument isn't a valid number, return undefined.
+*/
+
+const addTogether = () => {
+
+    
+}
